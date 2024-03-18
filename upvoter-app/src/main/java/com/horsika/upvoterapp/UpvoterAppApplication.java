a3933ef95eef30a36800bb0ccf9763c51b04bb85
horsika <45226821+horsika@users.noreply.github.com>
2024-03-18 10:20:45 +0100

    spring project init

---
+++ upvoter-app/src/main/java/com/horsika/upvoterapp/UpvoterAppApplication.java
@@ -0,0 +1,13 @@
+package com.horsika.upvoterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpvoterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpvoterAppApplication.class, args);
+	}
+
+}
---
+++ upvoter-app/src/test/java/com/horsika/upvoterapp/UpvoterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horsika.upvoterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpvoterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
